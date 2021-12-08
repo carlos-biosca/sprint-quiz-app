87a87f1f226f28aa22db97d80ffe435d165557ef
Carlos <cbiosca82gmail.com>
2021-12-08 14:28:50 +0100

    Game page part2

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import './Game.css'
 
 import { useGame } from '../../contexts/gameContext'
@@ -14,17 +16,58 @@ export default function Game () {
     console.log('info');
   }
 
+  //Tabs
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
   return (
     <div className="game">
       <div className="game__options" onClick={handleOptions}></div>
       <div className="select__info game__info" onClick={handleInfo}></div>
       <div className="game__wheel"></div>
-      <div className="game-players">
-        <ul>
-          {
-            names.map((name, index) => <li key={index}>{name}</li>)
-          }
-        </ul>
+      <div className="game__container">
+        <div className="game__tabs">
+          <button
+            className={toggleState === 1 ? "game__tab game__tab--active" : "game__tab"}
+            onClick={() => toggleTab(1)}
+          >
+            Carlos
+          </button>
+          <button
+            className={toggleState === 2 ? "game__tab game__tab--active" : "game__tab"}
+            onClick={() => toggleTab(2)}
+          >
+            Albert
+          </button>
+          <button
+            className={toggleState === 3 ? "game__tab game__tab--active" : "game__tab"}
+            onClick={() => toggleTab(3)}
+          >
+            <span className="game__name">Maria del carmen de todos los santos</span>
+          </button>
+          <button
+            className={toggleState === 4 ? "game__tab game__tab--active" : "game__tab"}
+            onClick={() => toggleTab(4)}
+          >
+            <span className="game__name">Jonathan</span>
+          </button>
+        </div>
+        <div className="game__records">
+          <div
+            className={toggleState === 1 ? "game__record  game__record--active" : "game__record"}
+          >
+            <h2>Content 1</h2>
+          </div>
+
+          <div
+            className={toggleState === 2 ? "game__record  game__record--active" : "game__record"}
+          >
+            <h2>Content 2</h2>
+          </div>
+        </div>
       </div>
     </div>
   )
