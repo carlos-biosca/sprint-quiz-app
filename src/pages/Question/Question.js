389901f8d3cab6f069afa8f0f2fe8fdd1b3e4c25
Carlos <cbiosca82gmail.com>
2021-12-10 14:14:34 +0100

    Question page part2

--- src/pages/Question/Question.js
+++ src/pages/Question/Question.js
@@ -1,9 +1,22 @@
 import './Question.css'
 
-export default function Question ({ move, screen, question }) {
+export default function Question ({ move, question }) {
   return (
-    <div className={!screen ? 'question' : 'question move-left'}>
-      <h2 onClick={move}>{question}</h2>
+    <div className="question">
+      <div className="question__header">
+        <h2 onClick={move} className='question__title'>{question}</h2>
+      </div>
+      <div className="question__text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt iure dignissimos laboriosam. Rerum maiores fugiat est sit animi nisi illo.</div>
+      <form className="question__form">
+        <div className="question__form-group">
+          <input type="radio" name="answer" id="1" className='question__form-input' />
+          <label htmlFor="1" className='question__form-label'>Answer 1</label>
+        </div>
+        <div className="question__form-group">
+          <input type="radio" name="answer" id="2" className='question__form-input' />
+          <label htmlFor="2" className='question__form-label'>Answer 2</label>
+        </div>
+      </form>
     </div>
   )
 }
