83237e49c2f2e0553814d128f073d15c2f89b8c6
Carlos <cbiosca82gmail.com>
2021-12-20 13:59:55 +0100

    Modal info part2

--- src/components/InfoModal/InfoModal.js
+++ src/components/InfoModal/InfoModal.js
@@ -48,18 +48,46 @@ export default function InfoModal ({ closeModal }) {
       </section>
       <h2 className='modal-info__title modal-info__title--margin'>Rules</h2>
       <section className="modal-info__section">
-        <p className="modal-info__text">
-          As soon as you have won a scoring wedge of each color, start making your way back to the center space and try to land there by exact count.
-          <br />
-          <br />
-          If you overshoot, keep playing, answering questions in the categories you land on, until you land on the center.
-          <br />
-          <br />
-          When you get to the center, the other players (without looking at any cards) decide from which category you will have to answer a final, deciding question.
-          <br />
-          <br />
-          As in the rest of the game, the level of question will depend upon your die roll.
-        </p>
+        <div className="modal-info__rule">
+          <h3 className='modal-info__subtitle'>Gameplay</h3>
+          <div className="modal-info__text">
+            On your turn:
+            <ol>
+              <li>Spin the wheel.</li>
+              <li>Answer the question!</li>
+            </ol>
+          </div>
+        </div>
+        <div className="modal-info__rule">
+          <h3 className='modal-info__subtitle'>Correct Answers</h3>
+          <p className="modal-info__text">
+            There are six different categories of questions.<br></br>
+            If you answer a question correctly a matching-colored record will be place in your score.<br></br>
+            Then spin the wheel again and take another turn.
+          </p>
+        </div>
+        <div className="modal-info__rule">
+          <h3 className='modal-info__subtitle'>Final Question</h3>
+          <p className="modal-info__text">
+            As soon as you have won a record of each color, you will have to answer a final, deciding question.<br></br>
+            This question will be of a higher difficulty.<br></br>
+            If you answer the final question, you win the game!
+          </p>
+        </div>
+        <div className="modal-info__rule">
+          <h3 className='modal-info__subtitle'>Single Player</h3>
+          <p className="modal-info__text">
+            Win the game before reach the maximum number of fails allowed.<br></br>
+            The counter will increase each time you answer wrong.
+          </p>
+        </div>
+        <div className="modal-info__rule">
+          <h3 className='modal-info__subtitle'>Multiplayer</h3>
+          <p className="modal-info__text">
+            Complete all colors and answer the final question first.<br></br>
+            If you answer wrong, you lose the turn.
+          </p>
+        </div>
       </section>
     </div>
   )
