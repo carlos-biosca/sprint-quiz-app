668060f22c3a72b468fb620af10e54e86da7e628
Carlos <cbiosca82gmail.com>
2022-01-06 16:22:01 +0100

    Wheel css part2

--- src/components/SpinWheel/SpinWheel.js
+++ src/components/SpinWheel/SpinWheel.js
@@ -4,12 +4,24 @@ export default function SpinWheel () {
   return (
     <div className="wheel__container">
       <div className='wheel__center' />
-      <div class="wheel__score wheel__score--entertainment"></div>
-      <div class="wheel__score wheel__score--history"></div>
-      <div class="wheel__score wheel__score--geography"></div>
-      <div class="wheel__score wheel__score--nature"></div>
-      <div class="wheel__score wheel__score--sports"></div>
-      <div class="wheel__score wheel__score--art"></div>
+      <div className="wheel__wrap wheel__wrap--entertainment">
+        <div className="wheel__score wheel__score--entertainment" />
+      </div>
+      <div className="wheel__wrap wheel__wrap--history">
+        <div className="wheel__score wheel__score--history" />
+      </div>
+      <div className="wheel__wrap wheel__wrap--geography">
+        <div className="wheel__score wheel__score--geography" />
+      </div>
+      <div className="wheel__wrap wheel__wrap--nature">
+        <div className="wheel__score wheel__score--nature" />
+      </div>
+      <div className="wheel__wrap wheel__wrap--sports">
+        <div className="wheel__score wheel__score--sports" />
+      </div>
+      <div className="wheel__wrap wheel__wrap--art">
+        <div className="wheel__score wheel__score--art" />
+      </div>
     </div>
   )
 }
