5bae8f53e4333223898aaf3fd8c2d6dc3f4d54ca
Carlos <cbiosca82gmail.com>
2022-01-16 20:04:33 +0100

    Desktop media queries part4

--- src/components/TransitionBackground/TransitionBackground.js
+++ src/components/TransitionBackground/TransitionBackground.js
@@ -5,23 +5,65 @@ import { world, video, monalisa, scroll, running, plant } from '../../data'
 export default function TransitionBackground () {
   return (
     <div className='transition'>
-      <div className='transition__category transition__category--entertainment'>
-        <img src={video} alt="" className="transition__icon" />
+      <div className="transition__column">
+        <div className='transition__category transition__category--entertainment'>
+          <img src={video} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--history'>
+          <img src={scroll} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--geography'>
+          <img src={world} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--nature'>
+          <img src={plant} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--sports'>
+          <img src={running} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--art'>
+          <img src={monalisa} alt="" className="transition__icon" />
+        </div>
       </div>
-      <div className='transition__category transition__category--history'>
-        <img src={scroll} alt="" className="transition__icon" />
+      <div className="transition__column">
+        <div className='transition__category transition__category--entertainment'>
+          <img src={world} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--history'>
+          <img src={plant} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--geography'>
+          <img src={monalisa} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--nature'>
+          <img src={running} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--sports'>
+          <img src={scroll} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--art'>
+          <img src={video} alt="" className="transition__icon" />
+        </div>
       </div>
-      <div className='transition__category transition__category--geography'>
-        <img src={world} alt="" className="transition__icon" />
-      </div>
-      <div className='transition__category transition__category--nature'>
-        <img src={plant} alt="" className="transition__icon" />
-      </div>
-      <div className='transition__category transition__category--sports'>
-        <img src={running} alt="" className="transition__icon" />
-      </div>
-      <div className='transition__category transition__category--art'>
-        <img src={monalisa} alt="" className="transition__icon" />
+      <div className="transition__column">
+        <div className='transition__category transition__category--entertainment'>
+          <img src={monalisa} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--history'>
+          <img src={running} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--geography'>
+          <img src={video} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--nature'>
+          <img src={scroll} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--sports'>
+          <img src={world} alt="" className="transition__icon" />
+        </div>
+        <div className='transition__category transition__category--art'>
+          <img src={plant} alt="" className="transition__icon" />
+        </div>
       </div>
     </div>
   )
